29ca34356762b239991cf27012a96ec12616f9fb
zmg <katteiger@gmail.com>
2025-12-10 16:02:02 +0800

    add oled function

---
+++ oled.py
@@ -0,0 +1,20 @@
+from machine import Pin, SoftI2C
+import ssd1306
+
+class
+# 创建i2c对象
+i2c = SoftI2C(scl=Pin(22), sda=Pin(21))
+
+# 宽度高度
+oled_width = 128
+oled_height = 64
+
+# 创建oled屏幕对象
+oled = ssd1306.SSD1306_I2C(oled_width, oled_height, i2c)
+
+# 在指定位置处显示文字
+oled.text('Hello!', 0, 0)
+oled.text('Hello, World 2!', 0, 20)
+oled.text('1231', 0, 40)
+        
+oled.show()
